eb5513481d2427777844e9b0943f80fd603ac4ac
Your Name <you@example.com>
2022-01-28 16:09:26 +0530

    Added plymorphism definition file

---
+++ polymorphism.cpp
@@ -0,0 +1,16 @@
+// Many forms
+/*
+There are two types of polymorphisms
+1.Compile time polymorphism ->function and operator overloading
+2. Run time polymorphism -> virtual fundtions
+
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
